3be9f67f030c8f1cad95ababfb9ca919a2b75043
Zeed06 <botakgantengg12@gmail.com>
2026-03-24 07:20:58 +0700

    Delete back-end directory

--- back-end/src/routes/follow.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from 'hono';
-import { toggleFollow } from '../controllers/follow.controller';
-import { authMiddleware, UserContext } from '../middleware/authMiddleware';
-
-const followRoutes = new Hono<UserContext>();
-
-followRoutes.post('/:id/follow', authMiddleware, toggleFollow);
-
-export default followRoutes;
